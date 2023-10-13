cc08249f2943ffcdd122c4c7c1f8a1f266e72bba
Franco Poffo <franco_sutter@yahoo.com.br>
2023-10-13 09:16:33 -0300

    Tratamento do erro no login

--- components/Nav/auth/SignInForm.tsx
+++ components/Nav/auth/SignInForm.tsx
@@ -5,23 +5,43 @@ import GoogleButton from "./GoogleButton";
 import GitHubButton from "./GitHubButton";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
+import { NextApiResponse } from "next";
 
 const SignInForm = () => {
   const [loginData, setLoginData] = useState({
     email: "",
     password: "",
   });
+  const [loginSuccess, setLoginSuccess] = useState(true);
+
+  let login;
 
   const { push } = useRouter();
 
   const onLoginHandler = async (e: FormEvent) => {
     e.preventDefault();
-    signIn("credentials", {
+    login = await signIn("credentials", {
       ...loginData,
       redirect: false,
     });
-    push("/");
+
+    console.log(login);
+
+    if (login!.ok) {
+      push("/");
+      setLoginSuccess(true);
+    } else {
+      setLoginSuccess(false);
+    }
   };
+
+  if (!loginSuccess) {
+    return (
+      <span className="my-64 flex justify-center text-lg font-bold text-white">
+        Login error! Use the correct credentials.
+      </span>
+    );
+  }
 
   return (
     <div className="flex flex-col items-center justity-center gap-8 mt-16 h-[450px] w-[600px] p-5 border border-slate-500 rounded-md">
